f9fb13030e107d1e7b1f1f18facfe3d18ecb7b29
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-11-21 09:12:27 +0100

    Refactor to extract lib/config.js
    Also simplify its code a lot using the `deep-extend` NPM package

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 'use strict';
 
 const Proxy = require('./lib/proxy'),
+      config = require('./lib/config'),
       process = require('process');
 
 /* The configuration file is where all the decision making happens. 
  * Take a look at default-config.js for detailed explanations
  */
-let config;
-try {
-    config = require(process.env['PROXY_CONFIG_FILE']);
-} catch (e) {
-    config = undefined;
-}
+let proxy = new Proxy(config.readConfigFile())
 
-let proxy = new Proxy(config)
 proxy.serve().then(function(config) {
     console.log('Listening on port ' + config.proxy.port);
     console.log('Configuration summary: ', JSON.stringify(config, null, 4))
